b394cf7d96b3ebbedc8c197c1d3eb7f17eb81e00
alvaro911 <algp232@hotmail.com>
2017-06-29 17:03:40 -0600

    added new routes to the application

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Header from './Header';
-import Home from './Home'
-import CreateTournament from './CreateTournament'
+import Home from './Home';
+import CreateTournament from './CreateTournament';
+import SearchTournament from './SearchTournament';
+import RegisterTeam from './RegisterTeam';
 import './App.css';
 
 class App extends Component {
@@ -15,6 +17,8 @@ class App extends Component {
           <main>
             <Route exact path="/" component={Home}/>
             <Route exact path="/create-tournament" component={CreateTournament}/>
+            <Route exact path="/search-tournament" component={SearchTournament}/>
+            <Route exact path="/register-team" component={RegisterTeam}/>
           </main>
         </div>
       </Router>
---
+++ src/components/SearchTournament.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import './SearchTournament.css'
+
+export default function SearchTournament() {
+  return (
+    <div className="search-tournament">
+      <h2 className="Anton">Search for a league</h2>
+      <p>Ready to show the world what you and your friends are made of? Look at all the tournaments in your area and pick the one you would like to participate in. Pay attenttion when we the tournament starts. Are you looking for a tournament in particular? Use the search bar to find the one you are looking for, you can look for Tournament Name or by tournament creator name
+      </p>
+
+      <form className="tournament-form">
+        <label>State</label>
+        <input className="location" type="text" />
+        <label>City</label>
+        <input className="location" type="text" />
+        <label>Zip code</label>
+        <input className="location" type="text" />
+        <label>Search</label>
+        <input type="text" />
+      </form>
+      <div className="tournament-list">
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+        <div className="tournament-card">
+          <h3>Name of the tournament</h3>
+          <p>Teams in the league</p>
+          <p>4/10</p>
+          <p>City: Denver</p>
+          <h5>Created by: John Doe</h5>
+        </div>
+      </div>
+    </div>
+  )
+};
